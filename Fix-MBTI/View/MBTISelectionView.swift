0008dfb5293adfdf124a29628beead0a9e0dde53
olcrpo <lsg001025@gmail.com>
2025-02-04 17:23:43 +0900

    환경설정페이지 및 mbti피커뷰

--- Fix-MBTI/View/MBTISelectionView.swift
+++ Fix-MBTI/View/MBTISelectionView.swift
@@ -6,13 +6,86 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct MBTISelectionView: View {
+    @AppStorage("isFirstLaunch") private var isFirstLaunch: Bool = true
+    
+    @State private var currentMBTI = ["E", "N", "T", "P"]
+    @State private var targetMBTI = ["E", "N", "T", "P"]
+    
+    let mbtiOptions = [
+        ["E", "I"], // 외향형 vs 내향형
+        ["N", "S"], // 직관형 vs 감각형
+        ["T", "F"], // 사고형 vs 감정형
+        ["P", "J"]  // 인식형 vs 판단형
+    ]
+    
     var body: some View {
-        Text("앱 처음 실행 시 한번만 나타나는 MBTI 설정 뷰")
+        NavigationView {
+            VStack {
+           
+                MBTIPicker(selection: $currentMBTI, options: mbtiOptions)
+                
+                Image(systemName: "arrowshape.down.fill")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                
+                MBTIPicker(selection: $targetMBTI, options: mbtiOptions)
+                
+                Button("완료") {
+                    saveMBTI()
+                    isFirstLaunch = false
+                }
+                
+                .padding()
+                .buttonStyle(.borderedProminent)
+                .disabled(currentMBTI == targetMBTI) // 현재, 목표 mbti같을때 완료버튼 비활성화
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("MBTI 설정")
+                        .font(.headline)
+                }
+            }
+        }
+    }
+    
+    private func saveMBTI() {
+        let current = currentMBTI.joined() // "ENTP" 형식으로 변환
+        let target = targetMBTI.joined()
+        
+        let profile = MBTIProfile(currentMBTI: current, targetMBTI: target)
+        
+        let modelContext = try? ModelContainer(for: MBTIProfile.self).mainContext
+        modelContext?.insert(profile)
     }
 }
 
 #Preview {
     MBTISelectionView()
+}
+
+struct MBTIPicker: View {
+    @Binding var selection: [String]
+    let options: [[String]]
+    
+    var body: some View {
+        HStack {
+            ForEach(0..<4, id: \.self) { index in
+                Picker("", selection: $selection[index]) {
+                    ForEach(options[index], id: \.self) { option in
+                        Text(option)
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+                .pickerStyle(.wheel)
+                .frame(width: 60, height: 150)
+                .clipped()
+            }
+        }
+        .padding()
+    }
 }
