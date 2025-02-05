ca72b32215675f51300e1a62499eef2614ac65da
Junsoo Kim <kel_777@naver.com>
2025-02-05 10:50:36 +0900

    메인 뷰 간단 작업

--- Fix-MBTI/View/MissionView.swift
+++ Fix-MBTI/View/MissionView.swift
@@ -6,11 +6,45 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct MissionView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var missions: [Mission]
+    @State private var showAlert = false
+    
     var body: some View {
-        Text("메인뷰 르페메뉴")
+        NavigationStack {
+            List {
+                ForEach(missions) { mission in
+                    NavigationLink(destination: MissionDetailView(mission: mission)) {
+                        HStack {
+                            Text(mission.title)
+                        }
+                    }
+                }
+                .onDelete(perform: deleteMission)
+            }
+            .navigationTitle("나의 미션")
+            .toolbar {
+                Button(action: addMission) {
+                    Label("미션 추가", systemImage: "plus")
+                }
+            }
+        }
     }
+    
+    func addMission() {
+        let newMission = Mission(title: "즉흥적인 약속 잡기", detailText: "계획 없이 친구에게 연락해서 만나기", category: "P")
+        modelContext.insert(newMission)
+    }
+    
+    func deleteMission(offsets: IndexSet) {
+        for index in offsets {
+            modelContext.delete(missions[index])
+        }
+    }
+    
 }
 
 #Preview {
