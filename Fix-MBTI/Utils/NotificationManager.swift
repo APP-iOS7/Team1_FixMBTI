aa3b258c15d42ee939f7f0a40c0414bde2d670c4
Junsoo Kim <kel_777@naver.com>
2025-02-05 10:54:31 +0900

    알림 폴더 및 파일 추가

---
+++ Fix-MBTI/Utils/NotificationManager.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationManager.swift
+//  Fix-MBTI
+//  랜덤 알림 관리 파일
+//  Created by KimJunsoo on 2/5/25.
+//
+
+import Foundation
+import UserNotifications
+
+class NotificationManager {
+    
+}
